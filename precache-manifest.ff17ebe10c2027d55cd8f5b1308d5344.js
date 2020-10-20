a703dfa43b50b13b8da8e29732432e3fe5d05247
Kostya Sabada <kostyasabada@gmail.com>
2020-10-20 23:37:00 +0300

    Updates

---
+++ precache-manifest.ff17ebe10c2027d55cd8f5b1308d5344.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "48e77d275a482f11a6c0de93a687b746",
+    "url": "/redux-posts/index.html"
+  },
+  {
+    "revision": "0d8ccf4652a49728fa1c",
+    "url": "/redux-posts/static/js/2.61f7232e.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/redux-posts/static/js/2.61f7232e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dff8e7654630f672816d",
+    "url": "/redux-posts/static/js/main.cfae6bad.chunk.js"
+  },
+  {
+    "revision": "dd6ceca03ad013fa6b82",
+    "url": "/redux-posts/static/js/runtime-main.4ca0af0c.js"
+  }
+]);
